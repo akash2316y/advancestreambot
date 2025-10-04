5da258c7ee6ddef022101d72f9234337e2730144
akash2316y <iamakashrabha@gmail.com>
2025-10-04 12:06:00 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,15 +9,15 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '27116256'))
     API_HASH = str(getenv('API_HASH', '89669f3ecf7d5257926a50701371100b'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6620784685:AAGLKiPLBWNlnBBEwcOSL-SZV-TEl1g_0yQ'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7626180904:AAGlwYhTH0gV-6HwGWUl_m6E8IkAv1AyLwo'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1003111618647'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002616161685'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '152.53.155.85'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5002159457").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "8110231942").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Sivam_uv'))
@@ -28,13 +28,13 @@ class Var(object):
     else:
         ON_HEROKU = False
 
-    FQDN = str(getenv('FQDN', 'advancestreambot-11kq.onrender.com')) if not ON_HEROKU or getenv('FQDN') else APP_NAME + '.herokuapp.com'
+    FQDN = str(getenv('FQDN', 'https://152.53.155.85')) if not ON_HEROKU or getenv('FQDN') else APP_NAME + '.herokuapp.com'
     HAS_SSL = bool(getenv('HAS_SSL', False))
 
     if HAS_SSL:
-        URL = "https://advancestreambot-11kq.onrender.com/".format(FQDN)
+        URL = "https://152.53.155.85/".format(FQDN)
     else:
-        URL = "https://advancestreambot-11kq.onrender.com/".format(FQDN)
+        URL = "https://152.53.155.85/".format(FQDN)
 
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Marvelbotz:Marvelbotz@cluster0.oyxdumi.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'marvelbackupp'))
