4fdc6287e608d754dd00dd6f4245e999fab3a677
akash2316y <iamakashrabha@gmail.com>
2025-10-04 14:47:16 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -13,7 +13,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002616161685'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002917810501'))
     PORT = int(getenv('PORT', 8282))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
